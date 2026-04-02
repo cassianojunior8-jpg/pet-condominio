ed542d5a395269f041be3175213deb1a0b63ede1
cassianojunior8-jpg <cassianojunior8@gmail.com>
2026-04-01 22:45:03 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
+    require: true,
     rejectUnauthorized: false
-  },
-  family: 4 // 👈 FORÇA IPv4
+  }
 });
 
 module.exports = pool;
